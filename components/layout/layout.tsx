da4ae435abf7d5991d917c1ef950b87eb5942eba
Matthew Finucane <matfin@gmail.com>
2020-08-11 21:05:51 +0100

    feat: set up app metadata, icons, manifest and add new photos

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { MDXProvider } from '@mdx-js/react';
-import { components, GlobalStyle } from 'styles';
+import { colours, components, GlobalStyle } from 'styles';
 import { Container, LayoutHeader, Main, Nav } from './layout.css';
 
 export interface Props {
@@ -9,6 +10,7 @@ export interface Props {
 }
 
 const Layout = ({ children }: Props): JSX.Element => {
+  const { asPath } = useRouter();
   const [navRevealed, setNavRevealed] = useState<boolean>(false);
   const setBodyOverflow = (overflow: boolean): void => {
     document.body.style.overflow = overflow ? 'auto' : 'hidden';
@@ -26,8 +28,36 @@ const Layout = ({ children }: Props): JSX.Element => {
     <>
       <Head>
         <title>Cinematt</title>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="default-src 'self' 'unsafe-eval'; style-src 'unsafe-inline'; img-src 'self' *.cloudinary.com;"
+        />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
+        <meta name="theme-color" content={colours.secondary} />
         <meta name="description" content="Personal photography website of Matt Finucane" />
         <meta name="author" content="Matt Finucane" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="canonical" href={`https://cinematt.photography${asPath}`} />
+        <meta property="og:url" content={`https://cinematt.photography${asPath}`} />
+        <meta property="og:site_name" content="cinematt.photography" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en-IE" />
+        <meta property="og:title" content="Cinematt" />
+        <meta property="og:description" content="Personal photography website of Matt Finucane" />
+
+        <meta name="twitter:site" content="@matfinucane" />
+        <meta name="twitter:creator" content="@matfinucane" />
+        <meta name="twitter:title" content="Cinematt" />
+        <meta name="twitter:url" content={`https://cinematt.photography${asPath}`} />
+        <meta name="twitter:description" content="Personal photography website of Matt Finucane" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" href="/icons/logo-32.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/icons/logo-32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/icons/logo-128.png" sizes="128x128" />
+        <link rel="icon" type="image/png" href="/icons/logo-192.png" sizes="192x192" />
+        <link rel="icon" type="image/png" href="/icons/logo-196.png" sizes="196x196" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <MDXProvider components={components}>
         <Container>
--- styles/mixins.ts
+++ styles/mixins.ts
@@ -1,7 +1,7 @@
 import { css, FlattenSimpleInterpolation } from 'styled-components';
 import { breakpoints } from './vars';
 
-export const media = (Object.keys(breakpoints) as (keyof typeof breakpoints)[]).reduce((acc, label) => {
+export const media = (Object.keys(breakpoints) as (keyof typeof breakpoints)[]).reduce((acc, label: string) => {
   acc[label] = (st: string): FlattenSimpleInterpolation => css`
     @media (min-width: ${breakpoints[label]}px) {
       ${st}
