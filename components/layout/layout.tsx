b52994861721eec8aeb5dee80a022809b93689f4
Matt Finucane <matfin@users.noreply.github.com>
2020-08-23 19:53:06 +0100

    fix: update favicon images and correct path (#21)

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -32,13 +32,15 @@ const Layout = ({ children }: Props): JSX.Element => {
           httpEquiv="Content-Security-Policy"
           content="default-src 'self' 'unsafe-eval'; style-src 'unsafe-inline'; img-src 'self' *.cloudinary.com;"
         />
+        <meta name="apple-mobile-web-app-title" content="Cinematt" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
         <meta name="theme-color" content={colours.secondary} />
+
         <meta name="description" content="Personal photography website of Matt Finucane" />
         <meta name="author" content="Matt Finucane" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="canonical" href={`https://cinematt.photography${asPath}`} />
+
         <meta property="og:url" content={`https://cinematt.photography${asPath}`} />
         <meta property="og:site_name" content="cinematt.photography" />
         <meta property="og:type" content="website" />
@@ -51,13 +53,29 @@ const Layout = ({ children }: Props): JSX.Element => {
         <meta name="twitter:title" content="Cinematt" />
         <meta name="twitter:url" content={`https://cinematt.photography${asPath}`} />
         <meta name="twitter:description" content="Personal photography website of Matt Finucane" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" href="/icons/logo-32.png" sizes="16x16" />
-        <link rel="icon" type="image/png" href="/icons/logo-32.png" sizes="32x32" />
-        <link rel="icon" type="image/png" href="/icons/logo-128.png" sizes="128x128" />
-        <link rel="icon" type="image/png" href="/icons/logo-192.png" sizes="192x192" />
-        <link rel="icon" type="image/png" href="/icons/logo-196.png" sizes="196x196" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+
+        <meta name="msapplication-TileColor" content={colours.secondary} />
+        <meta name="msapplication-TileImage" content="/icons/ms-icon-144x144.png" />
+        <meta name="theme-color" content={colours.secondary} />
+
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="canonical" href={`https://cinematt.photography${asPath}`} />
       </Head>
       <MDXProvider components={components}>
         <Container>
