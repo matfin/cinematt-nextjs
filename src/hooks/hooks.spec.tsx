e1d42fc66a784aea2cb791384634892997235c17
Matt Finucane <matfin@users.noreply.github.com>
2020-09-07 19:38:38 +0100

    feat: add swipe to navigate between photos (#33)

--- src/hooks/hooks.spec.tsx
+++ src/hooks/hooks.spec.tsx
@@ -1,7 +1,9 @@
 import React, { useRef } from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import useIntersectionObserver from './useIntersectionObserver';
-import { intersectionObserverMock } from 'testutils';
+import useSwipe from './useSwipe';
+import { intersectionObserverMock, swipe } from 'testutils';
+import { Direction } from 'models/types';
 
 describe('userIntersectionObserver tests', () => {
   const spyDisconnect = jest.fn();
@@ -49,5 +51,70 @@ describe('userIntersectionObserver tests', () => {
     wrapper.unmount();
     expect(spyUnobserve).toHaveBeenCalled();
     expect(spyDisconnect).toHaveBeenCalled();
+  });
+});
+
+describe('useSwipe tests', () => {
+  const spyCb = jest.fn();
+  const TestComponent = (): JSX.Element => {
+    useSwipe(spyCb);
+
+    return <div />;
+  };
+
+  beforeEach((): void => {
+    spyCb.mockReset();
+  });
+
+  it('should swipe right', async (): Promise<void> => {
+    render(<TestComponent />);
+    swipe(Direction.Right);
+
+    expect(spyCb).toHaveBeenCalledWith(Direction.Right);
+  });
+
+  it('should swipe left', async (): Promise<void> => {
+    render(<TestComponent />);
+    swipe(Direction.Left);
+
+    expect(spyCb).toHaveBeenCalledWith(Direction.Left);
+  });
+
+  it('should swipe down', async (): Promise<void> => {
+    render(<TestComponent />);
+    swipe(Direction.Down);
+
+    expect(spyCb).toHaveBeenCalledWith(Direction.Down);
+  });
+
+  it('should swipe up', async (): Promise<void> => {
+    render(<TestComponent />);
+    swipe(Direction.Up);
+
+    expect(spyCb).toHaveBeenCalledWith(Direction.Up);
+  });
+
+  it('not act on smaller swipes', async (): Promise<void> => {
+    render(<TestComponent />);
+
+    fireEvent.touchStart(window, {
+      changedTouches: [
+        {
+          screenX: 20,
+          screenY: 20,
+        },
+      ],
+    });
+
+    fireEvent.touchEnd(window, {
+      changedTouches: [
+        {
+          screenX: 30,
+          screenY: 20,
+        },
+      ],
+    });
+
+    expect(spyCb).not.toHaveBeenCalled();
   });
 });
